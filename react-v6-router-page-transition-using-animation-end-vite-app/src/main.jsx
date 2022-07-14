5df80cc9f2bf326763eb2cba79e8197081acbc83
Higashi-Kota <higashi.kota@gri.jp>
2022-07-14 14:37:05 +0900

    add sample

--- react-v6-router-page-transition-using-animation-end-vite-app/src/main.jsx
+++ react-v6-router-page-transition-using-animation-end-vite-app/src/main.jsx
@@ -12,26 +12,62 @@ import {HomePage} from './pages/home';
 import {AboutPage} from './pages/about';
 import '@fontsource/inter';
 import './styles/index.scss';
+import {cx, css} from '@emotion/css';
+
+const presetTypeName = `roomToBottom`;
+// const presetTypeName = `fade`;
 
 const App = () => {
   const location = useLocation();
 
   const [displayLocation, setDisplayLocation] = useState(location);
-  const [transitionStage, setTransitionStage] = useState('fadeIn');
+  const [transitionStage, setTransitionStage] = useState(`${presetTypeName}In`);
 
   useEffect(() => {
+    // console.log(location, displayLocation)
     if (location !== displayLocation) {
-      setTransitionStage('fadeOut');
+      setTransitionStage(`${presetTypeName}Out`);
     }
-  }, [location, displayLocation]);
+  }, [location]);
+
+  console.log(transitionStage);
 
   return (
-    <div>
+    <div
+      className={css`
+        position: relative;
+      `}
+    >
       <div
-        className={`${transitionStage}`}
-        onAnimationEnd={() => {
-          if (transitionStage === 'fadeOut') {
-            setTransitionStage('fadeIn');
+        className={cx(
+          `${transitionStage}`,
+          css`
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            backface-visibility: hidden;
+            transform-style: preserve-3d;
+            transform: translate3d(0, 0, 0);
+            will-change: transform;
+          `
+        )}
+        onAnimationStart={(e) => {
+          // console.log(`onAnimationStart`);
+          const html = document.documentElement;
+          const body = html.querySelector('body');
+          html.classList.add('is-loading');
+          body.classList.add('is-loading');
+        }}
+        onAnimationEnd={(e) => {
+          // console.log(`onAnimationEnd`);
+          const html = document.documentElement;
+          const body = html.querySelector('body');
+          html.classList.remove('is-loading');
+          body.classList.remove('is-loading');
+          if (transitionStage === `${presetTypeName}Out`) {
+            setTransitionStage(`${presetTypeName}In`);
             setDisplayLocation(location);
           }
         }}
